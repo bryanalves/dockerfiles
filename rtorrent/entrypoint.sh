94e5311646bd5d7cf83f246feffda2462e77c080
Bryan Alves <bryanalves@gmail.com>
2018-04-24 22:49:25 -0400

    Separate/simplify configs

--- rtorrent/entrypoint.sh
+++ rtorrent/entrypoint.sh
@@ -5,16 +5,6 @@ addgroup --gid $(stat -c %g /data/download) torrent && useradd --home-dir /app -
 mkdir -p /data/rtorrent/session
 mkdir -p /data/rtorrent/torrents
 
-[ "${RTORRENT_BIND}" ] && {
-    sed -i '/^network.bind_address.set =' /app/rtorrent.rc
-    echo "network.bind_address.set = ${RTORRENT_BIND}" >> /app/rtorrent.rc
-}
-
-[ "${RTORRENT_LOCALADDR}" ] && {
-    sed -i '/^network.local_address.set =' /app/rtorrent.rc
-    echo "network.local_address.set = ${RTORRENT_LOCALADDR}" >> /app/rtorrent.rc
-}
-
 rm -f /data/rtorrent/session/rtorrent.lock
 
 exec "$@"
